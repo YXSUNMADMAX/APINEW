7401059e33569b3b90718b01041781b174e4d941
MADMAXPRO <10175102160@stu.ecnu.edu.cn>
2019-07-09 17:53:54 +0800

    hyperlink

--- APINEW/snippets/serializers.py
+++ APINEW/snippets/serializers.py
@@ -2,6 +2,60 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES,Author
 from django.contrib.auth.models import User
 
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
+    class Meta:
+        model = Snippet
+        fields = ('url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style')
+
+
+class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Author
+        fields = ('url', 'id', 'name','title')
+
+
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('url', 'id', 'username', 'snippets')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 class SnippetSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
@@ -66,3 +120,4 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'snippets')
+'''
--- APINEW/snippets/urls.py
+++ APINEW/snippets/urls.py
@@ -7,12 +7,12 @@ from django.conf.urls import include
 urlpatterns = [
     path('', views.api_root),
     path('snippets/', views.SnippetList.as_view(),name="snippets"),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(),name="snippet-detail"),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(),name="snippet-highlight"),
     path('authors/', views.AuthorList.as_view(),name="authors"),
-    path('authors/<int:pk>/', views.AuthorDetail.as_view()),
+    path('authors/<int:pk>/', views.AuthorDetail.as_view(),name="author-detail"),
     path('users/', views.UserList.as_view(),name="users"),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view(),name="user-detail"),
 
 ]
 
