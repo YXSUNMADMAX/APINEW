ecde03e4ee4cbf5756745c98b3059d57ab25bf38
MADMAXPRO <10175102160@stu.ecnu.edu.cn>
2019-07-09 16:00:06 +0800

    defined classes

--- APINEW/snippets/urls.py
+++ APINEW/snippets/urls.py
@@ -3,10 +3,10 @@ from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('snippet/', views.snippet_list),
-    path('snippet/<int:pk>/', views.snippet_detail),
-    path('author/', views.author_list),
-    path('author/<int:pk>/', views.author_detail),
+    path('snippet/', views.SnippetList.as_view()),
+    path('snippet/<int:pk>/', views.SnippetDetail.as_view()),
+    path('author/', views.AuthorList.as_view()),
+    path('author/<int:pk>/', views.AuthorDetail.as_view()),
 ]
 
 
--- APINEW/snippets/views.py
+++ APINEW/snippets/views.py
@@ -1,98 +1,109 @@
 from snippets.models import Snippet,Author
 from snippets.serializers import SnippetSerializer,AuthorSerializer
-from rest_framework import status
-from rest_framework.decorators import api_view
+from django.http import Http404
+from rest_framework.views import APIView
 from rest_framework.response import Response
-# Create your views here.
+from rest_framework import status
+
 
 
 
 
 #..................for snippet................#
-@api_view(['GET', 'POST'])
-def snippet_list(request, format=None):
+
+class SnippetList(APIView):
     """
-    List all code snippets, or create a new snippet.
+    List all snippets, or create a new snippet.
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk, format=None):
-    """
-    Retrieve, update or delete a code snippet.
-    """
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+
+class SnippetDetail(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 #.................for Author...............#
 
-@api_view(['GET', 'POST'])
-def author_list(request, format=None):
+class AuthorList(APIView):
     """
-    List all code authors, or create a new author.
+    List all authors, or create a new author.
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         authors = Author.objects.all()
         serializer = AuthorSerializer(authors, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = AuthorSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def author_detail(request, pk, format=None):
-    """
-    Retrieve, update or delete a code author.
-    """
-    try:
-        author = Author.objects.get(pk=pk)
-    except Author.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+
+
+
+class AuthorDetail(APIView):
+    """
+    Retrieve, update or delete a author instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Author.objects.get(pk=pk)
+        except Author.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        author = self.get_object(pk)
         serializer = AuthorSerializer(author)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        author = self.get_object(pk)
         serializer = AuthorSerializer(author, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        author = self.get_object(pk)
         author.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
